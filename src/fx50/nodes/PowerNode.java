06b39cc44b7539c3fbf789406e7730aa139e5d9c
Peter Tse <tsehonam@gmail.com>
2017-01-12 14:29:41 +0800

    Partially fixed power negative bug

--- src/fx50/nodes/PowerNode.java
+++ src/fx50/nodes/PowerNode.java
@@ -17,10 +17,22 @@ public class PowerNode implements CalculatorNode {
     }
 
     public BigDecimal evaluate() {
-        return BigDecimalMath.pow(
-                left.evaluate().setScale(15, BigDecimal.ROUND_HALF_UP),
-                right.evaluate().setScale(15, BigDecimal.ROUND_HALF_UP)
-        );
+        BigDecimal lft = left.evaluate().setScale(15, BigDecimal.ROUND_HALF_UP);
+        BigDecimal rgt = right.evaluate().setScale(15, BigDecimal.ROUND_HALF_UP);
+        if (lft.compareTo(BigDecimal.ZERO) >= 0) {
+            if (rgt.compareTo(BigDecimal.ZERO) == 0){
+                throw new ArithmeticException("0^0 is undefined.");
+            }
+            return BigDecimalMath.pow(lft, rgt);
+        } else {
+            if (rgt.remainder(new BigDecimal(2)).compareTo(BigDecimal.ZERO) == 0) {
+                return BigDecimalMath.pow(lft.negate(), rgt);
+            } else if (Math.abs(rgt.remainder(new BigDecimal(2)).compareTo(BigDecimal.ZERO)) == 1){
+                return BigDecimalMath.pow(lft.negate(), rgt).negate();
+            } else {
+                throw new ArithmeticException("negative int power float is not implemented yet.");
+            }
+        }
     }
 
     public String toString() {
